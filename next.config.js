30a9af8488f941bf4ba23a5d76aff55dfb2fc8ca
Raheel Junaid <raheelj2004@gmail.com>
2022-04-24 22:02:51 -0600

    Switch to strapi

--- next.config.js
+++ next.config.js
@@ -1,20 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
-
-const STUDIO_REWRITE = {
-  source: "/studio/:path*",
-  destination:
-    process.env.NODE_ENV === "development"
-      ? "http://localhost:3000/studio/:path*"
-      : "/studio/index.html",
 };
 
 module.exports = {
-  rewrites: () => [STUDIO_REWRITE],
-	images: {
-		domains: ['cdn.sanity.io']
-	},
-  nextConfig
+  images: {
+    domains: ["localhost", "portfolio-webs-backend.herokuapp.com"],
+  },
+  nextConfig,
 };
--- pages/index.js
+++ pages/index.js
@@ -1,59 +1,56 @@
 import Image from "next/image";
 import Wave from "react-wavify";
-import { FaGithub, FaYoutube } from "react-icons/fa";
+import { FaGithub, FaTwitter, FaYoutube } from "react-icons/fa";
 import { FiExternalLink } from "react-icons/fi";
 import * as Tabs from "@radix-ui/react-tabs";
 import { useState, useRef } from "react";
 import { animated, useTransition } from "react-spring";
-import client from "../lib/sanity";
-import { groq } from "next-sanity";
-
-const technologies = [
-  "JavaScript",
-  "React",
-  "Node.js",
-  "Next.js",
-  "Firebase",
-  "Python",
-];
+import axios from "axios";
 
 export async function getStaticProps() {
-  const work_xp_query = groq`
-  *[_type == "work-experience"] | order(present desc) {
-    _id,
-    job_title,
-    company_name,
-    company_full_name,
-    excerpt
+  try {
+    const headers = {
+      headers: { Authorization: `Bearer ${process.env.API_TOKEN}` },
+    };
+
+    let res = await axios.get(
+      `${process.env.BASE_URL}/api/homepage?populate=*`,
+      headers
+    );
+    const homePageData = res.data.data.attributes;
+
+    res = await axios.get(
+      `${process.env.BASE_URL}/api/work-experiences?sort=startDate:desc`,
+      headers
+    );
+    const workExperiences = res.data.data;
+
+    res = await axios.get(
+      `${process.env.BASE_URL}/api/projects?populate=*`,
+      headers
+    );
+    const projects = res.data.data;
+
+    return {
+      props: {
+        homePageData,
+        workExperiences,
+        projects,
+        BASE_URL: process.env.BASE_URL,
+      },
+    };
+  } catch (error) {
+    console.error(error.response?.data.error || error);
+    return { notFound: true };
   }
-  `;
-
-  const project_query = groq`
-  *[_type == "project"] | order(date desc) {
-    _id,
-    title,
-    description,
-    "imageUrl": image.asset->url,
-    technologies,
-    source_url,
-    demo_url,
-    date
-  }
-  `;
-
-  const workExperience = await client.fetch(work_xp_query);
-  const projects = await client.fetch(project_query);
-
-  return {
-    props: {
-      workExperience,
-      projects,
-    },
-    revalidate: 10,
-  };
 }
 
-export default function Home({ workExperience, projects }) {
+export default function Home({
+  homePageData,
+  workExperiences,
+  projects,
+  BASE_URL,
+}) {
   const [activeTab, setActiveTab] = useState(0);
   const previousTab = useRef(0);
   const tabTransitions = useTransition(activeTab, {
@@ -75,6 +72,30 @@ export default function Home({ workExperience, projects }) {
     keys: null,
   });
 
+  const technologies = [
+    "JavaScript",
+    "React",
+    "Node.js",
+    "Next.js",
+    "Firebase",
+    "Python",
+  ];
+
+  const socials = [
+    {
+      icon: <FaGithub />,
+      link: "https://github.com/raheelrjunaid",
+    },
+    {
+      icon: <FaYoutube />,
+      link: "https://www.youtube.com/c/raheeljunaid",
+    },
+    {
+      icon: <FaTwitter />,
+      link: "https://twitter.com/raheelrjunaid",
+    },
+  ];
+
   return (
     <main>
       <section className="h-screen pt-20 flex items-center">
@@ -85,17 +106,26 @@ export default function Home({ workExperience, projects }) {
             <br />
             <span className="text-slate-200">I build online experiences.</span>
           </h1>
-          <p className="text-slate-400 leading-relaxed mt-5 mb-10 w-2/3 lg:w-1/2">
-            I&apos;m a software engineer that empowers users through developing
-            applications. My apps/websites use full-stack web technologies to
-            enhance the user experience with minimal painpoints.
+          <p className="text-slate-400 leading-relaxed mt-5 mb-10 w-100 md:w-2/3 lg:w-1/2">
+            {homePageData.mainDescription}
           </p>
-          <div className="flex gap-x-4 items-center">
+          <div className="flex gap-x-7 items-center">
             <a href="#projects">
               <button className="transition bg-sky-600 shadow-none hover:shadow-sky-500/25 text-md lg:text-lg px-5 py-2 border-sky-700 hover:shadow-lg hover:bg-sky-500 rounded-lg">
                 Projects
               </button>
             </a>
+            {socials.map((icon, index) => (
+              <a
+                href={icon.link}
+                target="_blank"
+                rel="noopener noreferrer"
+                key={index}
+                className="text-sky-900 hover:text-sky-700 text-2xl"
+              >
+                {icon.icon}
+              </a>
+            ))}
           </div>
         </div>
       </section>
@@ -121,30 +151,17 @@ export default function Home({ workExperience, projects }) {
                 About me
               </h2>
               <p className="my-8 text-slate-400 leading-relaxed">
-                Welcome! My name is Raheel, and I enjoy creating modern web
-                applications. I originally started as a Python developer back in
-                2020 but have since grown to work with various tech stacks.
-                Today, I use Javascript, React.js, and Python to build
-                applications for the future (currently working at{" "}
-                <a
-                  href="https://skyit.services/"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="underline decoration-sky-500 underline-offset-1 text-slate-300 hover:text-sky-500 font-medium"
-                >
-                  SkyIT
-                </a>
-                ).
+                {homePageData.aboutDescription}
               </p>
               <p className="font-mono mb-3">Some technologies I use are:</p>
               <div className="flex gap-x-10 font-mono text-sky-400 my-2">
                 <ul className="list-disc list-inside">
-                  {technologies.slice(0, 3).map((tech, i) => (
-                    <li key={i}>{tech}</li>
+                  {homePageData.technologies.slice(0, 3).map(({ id, name }) => (
+                    <li key={id}>{name}</li>
                   ))}
                 </ul>
                 <ul className="list-disc list-inside">
-                  {technologies.slice(3).map((tech, i) => (
+                  {homePageData.technologies.slice(3).map((tech, i) => (
                     <li key={i}>{tech}</li>
                   ))}
                 </ul>
@@ -196,35 +213,35 @@ export default function Home({ workExperience, projects }) {
           >
             <div className="relative w-min mx-auto mb-10">
               <Tabs.List className="whitespace-nowrap">
-                {workExperience.map((experience, i) => (
+                {workExperiences.map(({ id, attributes }, index) => (
                   <Tabs.Trigger
-                    value={i}
-                    key={experience._id}
+                    value={index}
+                    key={id}
                     className={`transition py-3 w-20 ${
-                      activeTab != i && "opacity-75"
+                      activeTab != index && "opacity-75"
                     }`}
                   >
-                    {experience.company_name}
+                    {attributes.company}
                   </Tabs.Trigger>
                 ))}
               </Tabs.List>
               <animated.div
                 className="bg-white transition-all rounded h-0.5 w-20 absolute"
                 style={{
-                  left: `${(activeTab / workExperience.length) * 100}%`,
+                  left: `${(activeTab / workExperiences.length) * 100}%`,
                 }}
               />
             </div>
             {tabTransitions((props, tabIndex) => {
-              const { job_title, company_full_name, excerpt } =
-                workExperience[tabIndex];
+              const { company, title, excerpt } =
+                workExperiences[tabIndex].attributes;
               return (
                 <animated.div style={props}>
                   <h3 className="text-2xl mb-2 font-semibold text-slate-100">
-                    {job_title}
+                    {title}
                   </h3>
                   <p className="text-sky-400 font-mono text-sm mb-5">
-                    {company_full_name}
+                    {company}
                   </p>
                   <p className="text-slate-300 leading-relaxed">{excerpt}</p>
                 </animated.div>
@@ -245,14 +262,14 @@ export default function Home({ workExperience, projects }) {
           </div>
 
           <div className="px-0 sm:px-10 md:px-0 lg:px-10 grid md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-10">
-            {projects.map((project, i) => (
+            {projects.map(({ id, attributes }) => (
               <div
                 className="border border-slate-600/50 p-5 shadow-lg rounded-lg"
-                key={i}
+                key={id}
               >
                 <div className="relative aspect-video mb-5">
                   <Image
-                    src={project.imageUrl}
+                    src={`${BASE_URL}${attributes.image.data.attributes.url}`}
                     alt="Chat App"
                     objectFit="cover"
                     objectPosition="50% 50%"
@@ -262,22 +279,22 @@ export default function Home({ workExperience, projects }) {
                 </div>
                 <div className="flex justify-between items-center mb-3">
                   <h3 className="text-2xl font-semibold text-slate-100">
-                    {project.title}
+                    {attributes.title}
                   </h3>
                   <div className="flex gap-x-3 text-slate-600 text-2xl">
-                    {project.demo_url && (
+                    {attributes.demoUrl && (
                       <a
                         target="_blank"
-                        href={project.demo_url}
+                        href={attributes.demoUrl}
                         rel="noreferrer"
                       >
                         <FiExternalLink className="hover:text-slate-500 " />
                       </a>
                     )}
-                    {project.source_url && (
+                    {attributes.sourceUrl && (
                       <a
                         target="_blank"
-                        href={project.source_url}
+                        href={attributes.sourceUrl}
                         rel="noreferrer"
                       >
                         <FaGithub className="hover:text-slate-500 " />
@@ -286,10 +303,10 @@ export default function Home({ workExperience, projects }) {
                   </div>
                 </div>
                 <p className="text-slate-400 font-light leading-relaxed mb-5">
-                  {project.description}
+                  {attributes.description}
                 </p>
                 <p className="text-sm text-sky-300/80 font-mono">
-                  {project.technologies.join(" · ")}
+                  {attributes.technologies?.map(({ name }) => name).join(" · ")}
                 </p>
               </div>
             ))}
